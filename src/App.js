b390e0e69d8b142ef6493e54eede7d4ec0004d67
Irena <irenaskydive@gmail.com>
2019-02-13 22:42:22 +0100

    fetch restaurant opentable api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurants: [],
+            value: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+
+        this.fetchResults = this.fetchResults.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({ value: e.target.value });
+    }
+
+    onSearchSubmit(value){
+        // using the city parametar
+        this.fetchResults();
+    }
+
+    fetchResults(url){
+        fetch(`http://opentable.herokuapp.com/api/restaurants?city=${this.state.value}`)
+            .then(data => data.json())
+            .then((data) => {
+                this.setState((prev, props) => ({
+                    restaurants: data.restaurants
+                }));
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <br/>
+                <div className='search-container'>
+                    <div className='search-text'>Enter city name for restaurants.</div>
+                    <input type='text' className='search-bar' placeholder='Toronto' onChange={ this.handleChange }/>
+                    <input type='submit' className='search-submit' onClick={ this.onSearchSubmit }/>
+                </div>
+                <br/>
+
+                <div className='results-content'>
+                  {this.state.restaurants.map(restaurant => (
+                    <div  key={restaurant.id}>
+                      <div>Name: {restaurant.name}</div>
+                      <div>Address: {restaurant.address}, {restaurant.city}</div>
+                      <div>Price: {restaurant.price}</div>
+                      <br/>
+                    </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
