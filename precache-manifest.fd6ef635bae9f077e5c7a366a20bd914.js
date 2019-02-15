c91e6eb354f862369783c1c4735b902235bbcb50
Irena <irenaskydive@gmail.com>
2019-02-15 19:42:01 +0100

    Updates

---
+++ precache-manifest.fd6ef635bae9f077e5c7a366a20bd914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a58ab0397753f8d44243",
+    "url": "/react-restaurants/static/js/runtime~main.a58ab039.js"
+  },
+  {
+    "revision": "101c5058b638d4d64a32",
+    "url": "/react-restaurants/static/js/main.101c5058.chunk.js"
+  },
+  {
+    "revision": "cf08f870f21e9ad49e85",
+    "url": "/react-restaurants/static/js/2.cf08f870.chunk.js"
+  },
+  {
+    "revision": "101c5058b638d4d64a32",
+    "url": "/react-restaurants/static/css/main.b098ce2b.chunk.css"
+  },
+  {
+    "revision": "82e42694fc06fc64a473a63ec0f05106",
+    "url": "/react-restaurants/index.html"
+  }
+];
